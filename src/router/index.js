5dbebcf4329e8af82680c6407df739f5df03f017
ted <ted@fortrun.cn>
2017-05-16 15:08:10 +0800

    修改Enterprise路径

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Auth from '@/containers/Auth'
-import Main from '@/containers/Main'
+import Main from '@/containers/List'
 import Home from '@/containers/Home'
 
 // 企业路由
-import EnterpriseMain from '@/containers/Enterprise/main'
 import EnterpriseAdd from '@/containers/Enterprise/add'
 import EnterpriseEdit from '@/containers/Enterprise/edit'
-import EnterpriseGroup from '@/containers/Enterprise/group'
-import EnterpriseHotel from '@/containers/Enterprise/hotel'
+import EnterpriseMain from '@/containers/Enterprise/List/main'
+import EnterpriseGroup from '@/containers/Enterprise/List/group'
+import EnterpriseHotel from '@/containers/Enterprise/List/hotel'
 // 企业详情路由
-import EnterpriseGroupMain from '@/containers/Enterprise/Group/main'
-import EnterpriseGroupDetail from '@/containers/Enterprise/Group/detail'
-import EnterpriseGroupHotel from '@/containers/Enterprise/Group/hotel'
+import EnterpriseGroupMain from '@/containers/Enterprise/Detail/main'
+import EnterpriseGroupDetail from '@/containers/Enterprise/Detail/detail'
+import EnterpriseGroupHotel from '@/containers/Enterprise/Detail/hotel'
 
 Vue.use(Router)
 
@@ -33,33 +33,39 @@ const main = [{
       name: 'Enterprise',
       template: `<router-view/>`
     },
-    children: [{
-      path: '/',
-      component: EnterpriseMain,//企业菜单
-      children: [{
+    children: [
+      {
         path: '/',
-        component: EnterpriseGroup,
-      }, {
-        path: 'hotel',
-        component: EnterpriseHotel
-      }]
-    }, {
-      path: 'add',
-      component: EnterpriseAdd,
-    }, {
-      path: ':id/edit',
-      component: EnterpriseEdit,
-    }, {
-      path: ':id',
-      component: EnterpriseGroupMain,//企业详情菜单
-      children: [{
-        path: '/',
-        component: EnterpriseGroupDetail
-      }, {
-        path: 'hotel',
-        component: EnterpriseGroupHotel
-      }]
-    }]
+        component: EnterpriseMain,//企业菜单
+        children: [
+          {
+            path: '/',
+            component: EnterpriseGroup,
+          }, {
+            path: 'hotel',
+            component: EnterpriseHotel
+          }]
+      },
+      {
+        path: 'add',
+        component: EnterpriseAdd,
+      },
+      {
+        path: ':id/edit',
+        component: EnterpriseEdit,
+      },
+      {
+        path: ':id',
+        component: EnterpriseGroupMain,//企业详情菜单
+        children: [{
+          path: '/',
+          component: EnterpriseGroupDetail
+        }, {
+          path: 'hotel',
+          component: EnterpriseGroupHotel
+        }]
+      }
+    ]
   }]
 }]
 
