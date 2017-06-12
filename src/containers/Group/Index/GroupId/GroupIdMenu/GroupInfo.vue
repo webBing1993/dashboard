fe4ba31bc560feb3b1f2c71400dbc1198cc821c9
王旭 <wayshon@wangxudeMacBook-Pro.local>
2017-06-12 14:13:20 +0800

    no message

--- src/containers/Group/Index/GroupId/GroupIdMenu/GroupInfo.vue
+++ src/containers/Group/Index/GroupId/GroupIdMenu/GroupInfo.vue
@@ -25,7 +25,7 @@
               <el-input class="el-right" v-model="groupWeb" placeholder="选填，请输入企业官网" :disabled="!isEditInfo"></el-input>
             </div>
             <div class="button-box" v-show="isEditInfo">
-              <el-button class="el-btn" type="success" @click.native="nextStep">修改</el-button>
+              <el-button class="el-btn" type="success" @click.native="nextStep">确认修改</el-button>
               <el-button @click.native="cancelInfo">取消</el-button>
             </div>
           </div>
