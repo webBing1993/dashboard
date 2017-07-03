248618884ed63a8170780079a13e855ed32d1c6a
王旭 <wayshon@wangxudeMacBook-Pro.local>
2017-07-03 16:56:11 +0800

    预登记短信文字写错

--- src/containers/Group/Index/HotelId/HotelMenu/Config/ConfigInfo.vue
+++ src/containers/Group/Index/HotelId/HotelMenu/Config/ConfigInfo.vue
@@ -147,8 +147,8 @@
               <img src="../../../../../../assets/images/登记.png" alt="a">
             </div>
             <div class="item-text">
-              <span>预登记</span>
-              <p>配置客人是否可以预登记。</p>
+              <span>预登记短信</span>
+              <p>配置客人是否可以受到预登记短信提醒。</p>
             </div>
             <span class="tag_text"
                   :class="{'tag_text_red': !enabledPreCheckinSms, 'tag_text_green': enabledPreCheckinSms}">{{enabledPreCheckinSms ? '已开通' : '未开通'}}</span>
