4f44b99fe09a4476bf69bdfc62dff5d05bf21df8
Zoe <zoe.zheng@fortrun.cn>
2018-01-23 16:29:15 +0800

    zoe pms暂存

--- config/index.js
+++ config/index.js
@@ -30,9 +30,9 @@ module.exports = {
     // proxyTable: {},
     proxyTable: {
       '/virgo': {
-        target: 'https://intg.fortrun.cn/',   //联调
+        // target: 'https://intg.fortrun.cn/',   //联调
         // target: 'https://qa.fortrun.cn/',  //提交测试
-        // target: 'https://gem.fortrun.cn/',
+        target: 'https://gem.fortrun.cn/',
         changeOrigin: true,
         pathRewrite: {
           // '^/virgo': ''
@@ -40,8 +40,8 @@ module.exports = {
       },
       '/libra': {
         // target: 'https://intg.fortrun.cn/',
-        target: 'https://qa.fortrun.cn/',
-        // target: 'https://gem.fortrun.cn/',
+        // target: 'https://qa.fortrun.cn/',
+        target: 'https://gem.fortrun.cn/',
         changeOrigin: true,
         pathRewrite: {
           // '^/virgo': ''
