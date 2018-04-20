0971eeb22620c0a337ce6f25416a4a53eb682912
zoeZheng <1423736181@qq.com>
2018-04-20 15:14:09 +0800

    关键通道显示

--- src/containers/Group/Index/HotelId/HotelMenu/Config/ConfigInfo.vue
+++ src/containers/Group/Index/HotelId/HotelMenu/Config/ConfigInfo.vue
@@ -617,20 +617,20 @@
             </span>
           </button>
         </el-col>
-        <!--<el-col :span="8">-->
-          <!--<button @click="dialogConfig(enumShowType.keyAccess)">-->
-            <!--<div class="item_img">-->
-              <!--<img src="../../../../../../assets/images/标签.png" alt="a">-->
-            <!--</div>-->
-            <!--<div class="item-text">-->
-              <!--<span>关键通道配置</span>-->
-              <!--<p>配置关键通道</p>-->
-            <!--</div>-->
-            <!--<span class="tag_text"-->
-                  <!--:class="{'tag_text_red': !configData.enable_pull_identity_guest_info , 'tag_text_green': configData.enable_pull_identity_guest_info }">{{configData.enable_pull_identity_guest_info ? '已配置' : '未配置'}}-->
-            <!--</span>-->
-          <!--</button>-->
-        <!--</el-col>-->
+        <el-col :span="8">
+          <button @click="dialogConfig(enumShowType.keyAccess)">
+            <div class="item_img">
+              <img src="../../../../../../assets/images/标签.png" alt="a">
+            </div>
+            <div class="item-text">
+              <span>关键通道配置</span>
+              <p>配置关键通道</p>
+            </div>
+            <span class="tag_text"
+                  :class="{'tag_text_red': !configData.enable_pull_identity_guest_info , 'tag_text_green': configData.enable_pull_identity_guest_info }">{{configData.enable_pull_identity_guest_info ? '已配置' : '未配置'}}
+            </span>
+          </button>
+        </el-col>
       </el-row>
       <!--/弹框页-->
       <el-dialog
@@ -1773,16 +1773,16 @@
               </el-switch>
             </div>
           </div>
-          <!--<div v-if="showType === enumShowType.keyAccess">-->
-            <!--<div class="item-form">-->
-              <!--<span>关键通道开关</span>-->
-              <!--<el-switch-->
-                <!--v-model="enableKeyAccess"-->
-                <!--on-color="#13ce66"-->
-                <!--off-color="#ff4949">-->
-              <!--</el-switch>-->
-            <!--</div>-->
-          <!--</div>-->
+          <div v-if="showType === enumShowType.keyAccess">
+            <div class="item-form">
+              <span>关键通道开关</span>
+              <el-switch
+                v-model="enableKeyAccess"
+                on-color="#13ce66"
+                off-color="#ff4949">
+              </el-switch>
+            </div>
+          </div>
         </div>
         <!--footer-->
         <div slot="footer" class="dialog-footer" v-if="switchName === 'close' && delName==='close'">
@@ -1878,8 +1878,8 @@
     // appManage:45,
     appManage2:45,
     reviewRoomNum:46,
-    isShowPoliceHandeld:47
-    // keyAccess:47
+    isShowPoliceHandeld:47,
+    keyAccess:48
   }
 
   //弹框标题类型
@@ -1930,8 +1930,8 @@
     '无证核验',
     '应用功能配置管理',
     '旅业房间号核对配置',
-    '公安验证是否显示已处理记录'
-    // '关键通道配置',
+    '公安验证是否显示已处理记录',
+    '关键通道配置',
 
   ]
 
@@ -2710,9 +2710,9 @@
           case enumShowType.informCoResident:
             result=this.validateInformCoResident;
             break;
-          case enumShowType.appManage:
-            result=this.validateAppManage;
-            break;
+          // case enumShowType.appManage:
+          //   result=this.validateAppManage;
+          //   break;
           case enumShowType.appManage2:
               result=this.validateAppManage2;
               break;
