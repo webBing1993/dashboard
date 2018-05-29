5ef5916ac75f64657207c7cff525126883573d02
sunshujing <sunshujing@fortrun.cn>
2018-05-29 10:51:06 +0800

    qa放出来充值模块

--- src/containers/Group/Index/HotelId/HotelMenu.vue
+++ src/containers/Group/Index/HotelId/HotelMenu.vue
@@ -8,7 +8,7 @@
       <router-link :to="'/group/' + groupId + '/hotel/' + hotelId + '/receptionArea'" v-if="showReception">接待区</router-link>
       <a style="color: #4A4A4A;font-size: 16px;margin-top: 10px;font-weight: 600">配置管理</a>
       <router-link :to="'/group/' + groupId + '/hotel/' + hotelId + '/config'">门店配置</router-link>
-      <!--<router-link :to="'/group/' + groupId + '/hotel/' + hotelId + '/config2'">门店配置2</router-link>-->
+      <router-link :to="'/group/' + groupId + '/hotel/' + hotelId + '/config2'">门店配置2</router-link>
       <router-link :to="'/group/' + groupId + '/hotel/' + hotelId + '/ConfigProd'">开通产品配置</router-link>
       <a style="color: #4A4A4A;font-size: 16px;margin-top: 10px;font-weight: 600">门店设备</a>
       <router-link :to="'/group/' + groupId + '/hotel/' + hotelId + '/device'">设备管理</router-link>
@@ -17,10 +17,10 @@
       <router-link :to="'/group/' + groupId + '/hotel/' + hotelId + '/hotelStaff'">酒店员工</router-link>
       <a style="color: #4A4A4A;font-size: 16px;margin-top: 10px;font-weight: 600">产品版本管理</a>
       <router-link :to="'/group/' + groupId + '/hotel/' + hotelId + '/manageProdVersion'">产品版本</router-link>
-      <!--<a style="color: #4A4A4A;font-size: 16px;margin-top: 10px;font-weight: 600">充值</a>-->
-      <!--<router-link :to="'/group/' + groupId + '/hotel/' + hotelId + '/rechargeDetail'">充值详情</router-link>-->
-      <!--<router-link :to="'/group/' + groupId + '/hotel/' + hotelId + '/withoutCardCheck'">无证核验使用详情</router-link>-->
-    <!---->
+      <a style="color: #4A4A4A;font-size: 16px;margin-top: 10px;font-weight: 600">充值</a>
+      <router-link :to="'/group/' + groupId + '/hotel/' + hotelId + '/rechargeDetail'">充值详情</router-link>
+      <router-link :to="'/group/' + groupId + '/hotel/' + hotelId + '/withoutCardCheck'">无证核验使用详情</router-link>
+
     </aside>
     <router-view class="fifth-router"/>
   </div>
